af2e164035b97c16c161755b273187a49959a492
naybala <naybalaismylife.nbl@gmail.com>
2022-06-15 19:24:13 +0630

    adding modal ui

--- src/components/Pizza.vue
+++ src/components/Pizza.vue
@@ -12,7 +12,83 @@
     <td>{{ product.waiting_time }} min</td>
     <td>{{ product.description }}</td>
     <td class="text-center">
-      <button class="btn btn-primary me-1" @click="onEdit">Edit</button>
+      <!-- Button trigger modal -->
+      <button
+        type="button"
+        class="btn btn-primary me-1"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        @click="onEdit"
+      >
+        Edit
+      </button>
+
+      <!-- Modal -->
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Edit Something</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body text-start">
+              <div class="container">
+                <form>
+                  <div class="mb-3">
+                    <label class="form-label">Name</label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="mb-3">
+                    <label class="form-label">Price</label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="mb-3">
+                    <label class="form-label">Publish Status</label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="mb-3">
+                    <label class="form-label">Category</label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="mb-3">
+                    <label class="form-label">Discount</label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="mb-3">
+                    <label class="form-label">Buy One Get One</label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="mb-3">
+                    <label class="form-label">Waiting Time</label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="mb-3">
+                    <label class="form-label">Description</label>
+                    <input type="text" class="form-control" />
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <button class="btn btn-danger" @click="onDelete">Delete</button>
     </td>
   </tr>
