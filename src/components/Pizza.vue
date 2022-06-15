380aff14a5e2d9b870f1adf6710ac1f6dc314e8b
naybala <naybalaismylife.nbl@gmail.com>
2022-06-15 11:28:34 +0630

    commit

--- src/components/Pizza.vue
+++ src/components/Pizza.vue
@@ -7,7 +7,8 @@
     <td v-else>out of stock</td>
     <td>{{ product.category_id }}</td>
     <td>{{ product.discount_price }} mmk</td>
-    <td>{{ product.buy_one_get_one_status }}</td>
+    <td v-if="product.buy_one_get_one_status == 1">Buy One Get One</td>
+    <td v-else>Off</td>
     <td>{{ product.waiting_time }} min</td>
     <td>{{ product.description }}</td>
     <td class="text-center">
